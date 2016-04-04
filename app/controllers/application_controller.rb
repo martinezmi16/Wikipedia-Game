f912e9b613b413ab734326e975adee6500591c99
Michael <mmarti26@alumni.nd.edu>
2016-04-03 18:58:10 -0600

    Functionality for getting random topic

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,34 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def getTopics()
+
+
+    #Randomizes topics selected
+    Random.new_seed
+
+    topic_entries = Topic.find_each.entries
+    entry_count = topic_entries.length
+    topic1_index = rand(entry_count)
+    topic2_index = rand(entry_count)
+
+
+    # Ensures the topics are not the same
+    while( topic1_index == topic2_index )
+      topic2_index = rand( entry_count )
+    end
+
+
+    #Gets topics from list of topics
+    topic1 = topic_entries[topic1_index]
+    topic2 = topic_entries[topic2_index]
+
+    #Creates list for output
+    topic_list = [ topic1, topic2]
+
+    return topic_list
+
+  end
+
 end
--- app/controllers/gameplay_controller.rb
+++ app/controllers/gameplay_controller.rb
@@ -1,6 +1,17 @@
 class GameplayController < ApplicationController
   def countdown
 
+    topics = getTopics()
+
+    # Gets starting topics
+    @topic1 = topics[0]
+    @topic2 = topics[1]
+
+    # Gets names of topics to display
+    @topic1_name = @topic1.name
+    @topic2_name = @topic2.name
+
+
   end
 
   def wikigame
