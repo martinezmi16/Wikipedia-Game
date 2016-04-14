ae0105d832fb7c1ae0b992b672cc303ba694b4d4
Michael <mmarti26@alumni.nd.edu>
2016-04-13 18:08:37 -0600

    Added crude path saving.  This needs to be improved

--- app/controllers/gameplay_controller.rb
+++ app/controllers/gameplay_controller.rb
@@ -29,18 +29,22 @@ class GameplayController < ApplicationController
 
 
 
-    @time_left = params[:end].to_i - params[:start].to_i
+    @time_left = params[:end].to_i - params[:start].to_i + 2
 
 
     puts @time_left
 
+    @topic1 = params[:topic1]
+    @topic2 = params[:topic2]
+
     @start_obj = Topic.find_by_hyperlink( params[:topic1] )
     @end_obj = Topic.find_by_hyperlink( params[:topic2 ] )
-    current_path = params[:current_path]
-    @current_item = current_path.split("->").last
+    @current_path = params[:current_path]
+    @current_item = @current_path.split("->").last
 
-    if @current_item == @end_obj then
-      redirect_to :controller => "main", :action => "finished", :win => "true"
+    if @current_item == params[:topic2] then
+      redirect_to :controller => "main", :action => "finished", :win => "true", :time_left => @time_left,
+                  :topic1 => params[:topic1], :topic2 => params[:topic2], :current_path => params[:current_path]
     end
 
     @topic1_name = @start_obj.name
@@ -55,7 +59,16 @@ class GameplayController < ApplicationController
 
     current_db_links = Topic.select(:hyperlink)
 
+    current_paths = Path.select(:path)
+    if current_paths.include?(@current_path) == false
+      Path.create( :path => @current_path, :count => 1)
+    else
+      selected_path = Path.find_by_path(@current_path)
+      selected_path.increment(:count, 1)
+    end
+
     new_count = 0
+
 
     # Scrapes for new topics and adds to list
     @links.each do |link|
@@ -73,7 +86,10 @@ class GameplayController < ApplicationController
             #Gets url name
             db_url = local_url.split("/").last
 
-            #if db_url.include?(':') == false then
+            if db_url.include?(':') == false then
+
+
+
 
               temp_string =  request.url.sub('first=true', 'first=false') + "->" + db_url
               link['href'] = temp_string.sub(/start=[0-9]*/, "start=" + new_start.to_s)
@@ -89,9 +105,9 @@ class GameplayController < ApplicationController
               new_count += 1
 
 
-           # end
+           end
 
-          else
+          elsif !local_url.include?("#")
             link.remove
           end
 
--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -3,5 +3,14 @@ class MainController < ApplicationController
   end
 
   def finished
+
+    if params[:win] == "false" then
+      @welcome_text = "Sorry, You Lose :("
+    else
+      @welcome_text = "YOU WIN!"
+      @base_score = 100
+      @bonus = params[:time_left].to_i * 10
+      @final_score = @base_score + @bonus
+    end
   end
 end
